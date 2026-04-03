ea44a8e0f8fbd357031fc376ef1ecf2b22e6c26b
favoursui <samuelfavour858@gmail.com>
2026-04-03 21:38:47 +0100

    fix: updated dockerfile

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,11 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host      = "0.0.0.0",
-        port      = 8000,
-        reload    = True,
+        port      = int(os.environ.get("PORT", 8000)),
+        reload    = False,
         log_level = "info",
     )
